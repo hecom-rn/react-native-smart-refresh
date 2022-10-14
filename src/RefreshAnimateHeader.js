4509bb6c13119bbd450af9b42c2da7fc0c8cc7fb
lzt <liuzetan1988@126.com>
2022-10-14 15:07:05 +0800

    chore: android 使用系统的refreshcontrol

--- src/RefreshAnimateHeader.js
+++ src/RefreshAnimateHeader.js
@@ -1,77 +1,86 @@
 'use strict';
 import React, { useRef, useCallback, useState } from 'react';
-import { StyleSheet, Animated } from 'react-native';
+import { StyleSheet, Animated, RefreshControl } from 'react-native';
 import LottieView from 'lottie-react-native';
-import {SmartRefresh,SmartRefreshHeader} from "../index";
+import { SmartRefresh, SmartRefreshHeader } from "../index";
 
 function RefreshAnimateHeader(props) {
-  const { refreshing, onRefresh = () => {}, source='', containerStyle, lottieStyle, lottieOptions } = props;
+  if (Platform.OS == 'android') {
+    return (<RefreshControl
+      {...props}
+      colors={['#ff0000', '#00ff00', '#0000ff', '#3ad564']}
+      progressBackgroundColor="#ffffff"
+    />);
+}
 
-  const lottieRef = useRef(React.createRef());
-  let offsetValue = new Animated.Value(0);
-  let [currentState,setCurrentState] = useState(0);
-  const onRefreshCallBack = useCallback(
-    (state) => {
-        lottieRef.current?.play();
-        onRefresh(state);
-    },
-    [onRefresh],
-  );
 
-  const onIdleRefreshCallBack = useCallback((state) => {
-      lottieRef.current?.reset();
-  }, []);
-  const onChangeStateCallBack = useCallback((event) => {
-    const { state } = event.nativeEvent;
-    setCurrentState(state);
-    switch (state) {
-      case 0:
-          onIdleRefreshCallBack();
-        break;
-      case 2:
-        onRefreshCallBack();
-        break;
-      default:
+const { refreshing, onRefresh = () => { }, source = '', containerStyle, lottieStyle, lottieOptions } = props;
 
-    }
-  }, []);
-  const onChangeOffsetCallBack = useCallback((event) => {
-    const { offset } = event.nativeEvent;
+const lottieRef = useRef(React.createRef());
+let offsetValue = new Animated.Value(0);
+let [currentState, setCurrentState] = useState(0);
+const onRefreshCallBack = useCallback(
+  (state) => {
+    lottieRef.current?.play();
+    onRefresh(state);
+  },
+  [onRefresh],
+);
 
-    if (currentState == 0 || currentState == 1){
-      offsetValue.setValue(offset);
-    }
-  });
-  return (
-    <SmartRefresh
-      refreshing={refreshing}
-      onChangeState={onChangeStateCallBack}
-      onChangeOffset={onChangeOffsetCallBack}
+const onIdleRefreshCallBack = useCallback((state) => {
+  lottieRef.current?.reset();
+}, []);
+const onChangeStateCallBack = useCallback((event) => {
+  const { state } = event.nativeEvent;
+  setCurrentState(state);
+  switch (state) {
+    case 0:
+      onIdleRefreshCallBack();
+      break;
+    case 2:
+      onRefreshCallBack();
+      break;
+    default:
+
+  }
+}, []);
+const onChangeOffsetCallBack = useCallback((event) => {
+  const { offset } = event.nativeEvent;
+
+  if (currentState == 0 || currentState == 1) {
+    offsetValue.setValue(offset);
+  }
+});
+return (
+  <SmartRefresh
+    refreshing={refreshing}
+    onChangeState={onChangeStateCallBack}
+    onChangeOffset={onChangeOffsetCallBack}
+  >
+    <SmartRefreshHeader
+      style={[styles.container, { ...containerStyle }]}
     >
-      <SmartRefreshHeader
-        style={[styles.container, { ...containerStyle }]}
-      >
-        <LottieView
-          ref={lottieRef}
-          style={ [styles.lottery, { ...lottieStyle}] }
-          resizeMode={'cover'}
-          loop={true}
-          autoSize={false}
-          autoPlay={true}
-          speed={1}
-          source={source || require('./assets/cycle_animation.json')}
-          hardwareAccelerationAndroid={true}
-          cacheStrategy={'strong'}
-          progress={offsetValue.interpolate({
-            inputRange:[0,300],
-            outputRange:[0,1],
-          })}
-          {...lottieOptions}
-        />
-      </SmartRefreshHeader>
-      {props.children}
-    </SmartRefresh>
-  );
+      <LottieView
+        ref={lottieRef}
+        style={[styles.lottery, { ...lottieStyle }]}
+        resizeMode={'cover'}
+        loop={true}
+        autoSize={false}
+        autoPlay={true}
+        speed={1}
+        source={source || require('./assets/cycle_animation.json')}
+        hardwareAccelerationAndroid={true}
+        cacheStrategy={'strong'}
+        progress={offsetValue.interpolate({
+          inputRange: [0, 300],
+          outputRange: [0, 1],
+        })}
+        {...lottieOptions}
+      />
+    </SmartRefreshHeader>
+    {props.children}
+  </SmartRefresh>
+);
 }
 
 const styles = StyleSheet.create({
